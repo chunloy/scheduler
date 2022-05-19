8ec4e44fce28d52257cbde0eff07134ff4c1a3bf
Aaron Au <aaron.au@me.com>
2022-05-19 12:24:26 -0600

    Add axios as dependency and update package.json to use proxy

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 
+import DayList from "./DayList";
+import Appointment from "components/Appointment";
+import axios from "axios";
+
 import "components/Application.scss";
 
-import DayList from "./DayList";
 
 const days = [
   {
@@ -22,8 +25,64 @@ const days = [
   },
 ];
 
+const appointments = {
+  "1": {
+    id: 1,
+    time: "12pm",
+  },
+  "2": {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  "3": {
+    id: 3,
+    time: "2pm",
+  },
+  "4": {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  "5": {
+    id: 5,
+    time: "4pm",
+  },
+
+  "6": {
+    id: 6,
+    time: "5pm",
+  }
+};
+
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const appointmentList = Object.values(appointments).map(appointment => {
+    return (
+      <Appointment
+        key={appointment.id}
+        {...appointment}
+      />
+    );
+  });
+
+  axios.get('http://localhost:8001/api/days')
+    .then(res => console.log(res));
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -47,7 +106,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointmentList}
       </section>
     </main>
   );
