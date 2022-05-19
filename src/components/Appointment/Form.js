7c4b8c9d2914dc366e681fecd45db393b8008bde
Aaron Au <aaron.au@me.com>
2022-05-18 23:16:07 -0600

    implement form component with props and error handling

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+import Button from "components/Button";
+import InterviewerList from "components/InterviewerList";
+
+export default function Form(props) {
+  const reset = () => {
+    setStudent("");
+    setInterviewer(null);
+    props.onCancel();
+  };
+
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form onSubmit={event => event.preventDefault()} autoComplete="off">
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            value={student}
+            onChange={(event) => setStudent(event.target.value)}
+          />
+        </form>
+        <InterviewerList
+          interviewers={props.interviewers}
+          value={interviewer}
+          onChange={setInterviewer}
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button onClick={reset} danger>Cancel</Button>
+          <Button onClick={props.onSave} confirm>Save</Button>
+        </section>
+      </section>
+    </main>
+  );
+}
