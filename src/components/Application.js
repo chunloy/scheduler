ba82f77b6ccd6096eadfa1c81a86b0f2f6301ca2
Aaron Au <aaron.au@me.com>
2022-05-19 22:22:18 -0600

    refactor application.js to better manage multiple states

--- src/components/Application.js
+++ src/components/Application.js
@@ -46,8 +46,14 @@ const appointments = {
 };
 
 export default function Application(props) {
-  const [days, setDays] = useState([]);
-  const [day, setDay] = useState("Monday");
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+
+  const setDay = (day) => setState({ ...state, day });
+  const setDays = (days) => setState(prev => ({ ...prev, days }));
 
   const appointmentList = Object.values(appointments).map(appointment => {
     return (
@@ -74,8 +80,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            value={day}
+            days={state.days}
+            value={state.day}
             onChange={setDay}
           />
         </nav>
