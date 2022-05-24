aef8f6b96ed063670f30f129faec646a5d8cfba2
Aaron Au <aaron.au@me.com>
2022-05-24 14:42:10 -0600

    Define prop types in interviewer list component

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,10 +1,11 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import InterviewerListItem from "./InterviewerListItem";
 
 import "components/InterviewerList.scss";
 
-export default function InterviewerList(props) {
+function InterviewerList(props) {
   const interviewerListItems = props.interviewers.map(interviewer => {
     return (
       <InterviewerListItem
@@ -26,3 +27,9 @@ export default function InterviewerList(props) {
     </section>
   );
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
+
+export default InterviewerList;
