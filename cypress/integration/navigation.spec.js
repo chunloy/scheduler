8b42148339e1dbf881a1b8109557c86c1f918528
Aaron Au <aaron.au@me.com>
2022-05-25 17:33:06 -0600

    implement navigating days end to end test

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,5 +1,22 @@
 describe("Navigation", () => {
+  beforeEach(() => {
+    //create alias before each test
+    cy.visit("/").as("root");
+  });
+
   it("should visit root", () => {
-    cy.visit("/");
+    //visit root web page
+    cy.get("@root");
+  });
+
+
+  it("should navigate to Tuesday", () => {
+    //visit root webpage
+    cy.get("@root");
+
+    //find and click day list item tuesday
+    cy.contains("[data-testid=day]", "Tuesday")
+      .click()
+      .should("have.class", "day-list__item--selected");
   });
 });
